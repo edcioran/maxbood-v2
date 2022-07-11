6df4085ec6defdca930a27f305718605828bd87b
edcioran <muhfathiumar@gmail.com>
2022-07-11 14:50:03 +0700

    Added Navbar responsive color behavior for desktop

--- public/js/main.js
+++ public/js/main.js
@@ -3,6 +3,7 @@ let navbar = document.querySelector('#navbar')
 let navbarToggle = document.querySelector('#navbar-toggle')
 let mobileMenu = document.querySelector('#mobile-menu')
 let brandImg = navbar.firstElementChild.firstElementChild
+let navMenu = document.querySelectorAll('#navbar li a')
 
 function NavbarToWhite() {
     navbar.classList.replace('bg-transparent', 'bg-white')
@@ -14,6 +15,16 @@ function NavbarToTransparent() {
     navbar.classList.replace('bg-white', 'bg-transparent')
     navbarToggle.classList.replace('text-gray-500', 'text-white')
     brandImg.src = "../src/img/branding/logo-wo.png"
+}
+function NavbarDesktopMenuToBlack() {
+    navMenu.forEach(function(navA){
+        navA.classList.replace('md:text-white', 'md:text-gray-700')
+    })
+}
+function NavbarDesktopMenuToWhite() {
+    navMenu.forEach(function(navA){
+        navA.classList.replace('md:text-gray-700', 'md:text-white')
+    })
 }
 
 navbarToggle.addEventListener("click", function () {
@@ -30,6 +41,12 @@ navbarToggle.addEventListener("click", function () {
 
 // Navbar change color when scrolling
 document.addEventListener("scroll", function () {
-    if (window.scrollY >= 40) NavbarToWhite()
-    else if (window.scrollY < 40 && !mobileMenu.classList.contains('navbar-opened')) NavbarToTransparent()
+    if (window.scrollY >= 40) {
+        NavbarToWhite()
+        NavbarDesktopMenuToBlack()
+    }
+    else if (window.scrollY < 40 && !mobileMenu.classList.contains('navbar-opened')) {
+        NavbarToTransparent()
+        NavbarDesktopMenuToWhite()
+    }
 })
