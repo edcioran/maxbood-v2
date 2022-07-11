a3c130ec103633ccdf89dc3e989bdce6b48d70c2
edcioran <muhfathiumar@gmail.com>
2022-07-11 14:37:03 +0700

    Added Navbar color behavior on scroll

--- public/js/main.js
+++ public/js/main.js
@@ -1,20 +1,35 @@
-// Navbar change
+// Navbar change color
 let navbar = document.querySelector('#navbar')
-    let navbarToggle = document.querySelector('#navbar-toggle')
-    let mobileMenu = document.querySelector('#mobile-menu')
-    let brandImg = navbar.firstElementChild.firstElementChild
+let navbarToggle = document.querySelector('#navbar-toggle')
+let mobileMenu = document.querySelector('#mobile-menu')
+let brandImg = navbar.firstElementChild.firstElementChild
 
-    navbarToggle.addEventListener("click", function () {
-        // When navbar is NOT expanded
-        if (mobileMenu.classList.contains('hidden')) {
-            navbar.classList.replace('bg-transparent', 'bg-white')
-            navbarToggle.classList.replace('text-white', 'text-gray-500')
-            brandImg.src = "../src/img/branding/logo-bo.png"
-        }
-        else {
-            navbar.classList.replace('bg-white', 'bg-transparent')
-            navbarToggle.classList.replace('text-gray-500', 'text-white')
-            brandImg.src = "../src/img/branding/logo-wo.png"
-        }
+function NavbarToWhite() {
+    navbar.classList.replace('bg-transparent', 'bg-white')
+    navbar.classList.add('shadow-sm')
+    navbarToggle.classList.replace('text-white', 'text-gray-500')
+    brandImg.src = "../src/img/branding/logo-bo.png"
+}
+function NavbarToTransparent() {
+    navbar.classList.replace('bg-white', 'bg-transparent')
+    navbarToggle.classList.replace('text-gray-500', 'text-white')
+    brandImg.src = "../src/img/branding/logo-wo.png"
+}
 
-    })
+navbarToggle.addEventListener("click", function () {
+    // When navbar is NOT expanded
+    if (!mobileMenu.classList.contains('navbar-opened')) {
+        NavbarToWhite();
+        mobileMenu.classList.add('navbar-opened')
+    }
+    else {
+        if(window.scrollY<40) NavbarToTransparent()
+        mobileMenu.classList.remove('navbar-opened')
+    }
+})
+
+// Navbar change color when scrolling
+document.addEventListener("scroll", function () {
+    if (window.scrollY >= 40) NavbarToWhite()
+    else if (window.scrollY < 40 && !mobileMenu.classList.contains('navbar-opened')) NavbarToTransparent()
+})
