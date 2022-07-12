d2c87948cb07f6da85effdc79d440950e121760c
edcioran <muhfathiumar@gmail.com>
2022-07-12 15:42:35 +0700

    Added services.html

--- public/js/main.js
+++ public/js/main.js
@@ -1,17 +1,19 @@
 // Navbar change color
-let navbar = document.querySelector('#navbar')
-let navbarToggle = document.querySelector('#navbar-toggle')
-let mobileMenu = document.querySelector('#mobile-menu')
-let brandImg = navbar.firstElementChild.firstElementChild
-let navMenu = document.querySelectorAll('#navbar li a')
+const navbar = document.querySelector('#navbar')
+const navbarToggle = document.querySelector('#navbar-toggle')
+const mobileMenu = document.querySelector('#mobile-menu')
+const brandImg = navbar.firstElementChild.firstElementChild.firstElementChild
+const navMenu = document.querySelectorAll('#navbar li a')
 
 function NavbarToWhite() {
+    navbar.classList.add('is-white')
     navbar.classList.replace('bg-transparent', 'bg-white')
     navbar.classList.add('shadow-md')
     navbarToggle.classList.replace('text-white', 'text-gray-500')
     brandImg.src = "../src/img/branding/logo-bo.png"
 }
 function NavbarToTransparent() {
+    navbar.classList.remove('is-white')
     navbar.classList.replace('bg-white', 'bg-transparent')
     navbarToggle.classList.replace('text-gray-500', 'text-white')
     navbar.classList.remove('shadow-md')
@@ -42,11 +44,11 @@ navbarToggle.addEventListener("click", function () {
 
 // Navbar change color when scrolling
 document.addEventListener("scroll", function () {
-    if (window.scrollY >= 40) {
+    if (window.scrollY >= 40 && !navbar.classList.contains('is-white')) {
         NavbarToWhite()
         NavbarDesktopMenuToBlack()
     }
-    else if (window.scrollY < 40 && !mobileMenu.classList.contains('navbar-opened')) {
+    else if (window.scrollY < 40 && !mobileMenu.classList.contains('navbar-opened') && navbar.classList.contains('is-white')) {
         NavbarToTransparent()
         NavbarDesktopMenuToWhite()
     }
